85f953a2200585d3206c2c571d8abf149f259598
EoD <293499+EoD@users.noreply.github.com>
2025-07-10 21:46:34 +0000

    unify both stringToMap functions
    (cherry picked from commit 228b835c2aa6812bcdb486168d630bf4ffe19454)

--- src/main/java/vip/fubuki/playersync/util/LocalJsonUtil.java
+++ src/main/java/vip/fubuki/playersync/util/LocalJsonUtil.java
@@ -2,10 +2,11 @@ package vip.fubuki.playersync.util;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 public class LocalJsonUtil {
-    public static Map<String,String> StringToMap(String param) {
-        Map<String,String> map = new HashMap<>();
+    private static <K> Map<K, String> stringToGenericMap(String param, Function<String, K> keyParser) {
+        Map<K, String> map = new HashMap<>();
         String s1 = param.substring(1,param.length()-1);
         String s2 = s1.trim();
         String[] split = s2.split(",");
@@ -19,28 +20,16 @@ public class LocalJsonUtil {
 
             String key = trim.substring(0, equalIndex);
             String value = trim.substring(equalIndex + 1);
-            map.put(key, value);
+            map.put(keyParser.apply(key), value);
         }
         return map;
     }
 
-    public static Map<Integer,String> StringToEntryMap(String param) {
-        Map<Integer,String> map = new HashMap<>();
-        String s1 = param.substring(1,param.length()-1);
-        String s2 = s1.trim();
-        String[] split = s2.split(",");
-        for (int i = split.length - 1; i >= 0; i--) {
-            String trim = split[i].trim();
+    public static Map<String, String> StringToMap(String param) {
+        return stringToGenericMap(param, Function.identity());
+    }
 
-            // only check for the first "=" as the values also contain additional "="
-            int equalIndex = trim.indexOf('=');
-            if (equalIndex < 0)
-                continue;
-
-            String key = trim.substring(0, equalIndex);
-            String value = trim.substring(equalIndex + 1);
-            map.put(Integer.parseInt(key), value);
-        }
-        return map;
+    public static Map<Integer, String> StringToEntryMap(String param) {
+        return stringToGenericMap(param, Integer::parseInt);
     }
 }
